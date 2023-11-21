f0d83ff92f7b7a4620dc5465a2ca8b1287bf382b
Emily Nozaki <emiri3128.gmail.com>
2023-11-21 21:11:10 +0900

    Initial Commit

---
+++ AlertMapTeamA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlertMapTeamA
+//
+//  Created by Emily Nozaki on 2023/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
